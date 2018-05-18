7949c7549335308f199491bc231b9f3469baa853
Thanh Nguyen <ngtanthanh90@gmail.com>
2018-05-18 09:36:39 +0000

    Update Main Sheets 180518

--- admin/settings.php
+++ admin/settings.php
@@ -1,4 +1,4 @@
 <?php
-$sheetBangTinh  = '1Voi3pSkFzsoNfuFcLYS4ds7FzCepvXfUvda683Eu2HQ';
-$sheetDuAn      = '1ZLadObdGSdTPsZsNP8PqmZG2egYdPvRR11k8gkt5zcE';
+$sheetBangTinh  = '1NgZq41xShwrIkxDxX5XpWlI7QL0D8npnfN7slj_gIK0';
+$sheetDuAn      = '1m_zf3zUJa4iHemxzDSHPJ9KHhN0868ShNoeqc7tQ-kQ';
 ?>
