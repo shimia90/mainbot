c0d0048fd9ee273ad126f1d6698ca5cc23c23707
Thanh Nguyen <ngtanthanh90@gmail.com>
2018-05-18 09:22:39 +0000

    Fix Hide Keyboard 180518

--- types/init_keyboards.php
+++ types/init_keyboards.php
@@ -5,7 +5,7 @@ use unreal4u\TelegramAPI\Telegram\Types\ReplyKeyboardMarkup;
 $sendMessage->chat_id = A_USER_CHAT_ID;
 $sendMessage->text = 'Xin chào '.$firstName . ' ' . $lastName;
 $sendMessage->reply_markup = new ReplyKeyboardMarkup();
-$sendMessage->reply_markup->one_time_keyboard = true;
+//$sendMessage->reply_markup->one_time_keyboard = true;
 
 for($i = 0; $i < count($nutYeuCau); $i++) {
 	$keyboardButton = new KeyboardButton();
--- types/sua_thong_tin.php
+++ types/sua_thong_tin.php
@@ -7,7 +7,7 @@ $sendMessage->chat_id = A_USER_CHAT_ID;
 /*$sendMessage->text = getCurrentUserInfo(A_USER_CHAT_ID);*/
 $sendMessage->text = 'Vui lòng chọn các yêu cầu dưới đây:';
 $sendMessage->reply_markup = new ReplyKeyboardMarkup();
-$sendMessage->reply_markup->one_time_keyboard = true;
+//$sendMessage->reply_markup->one_time_keyboard = true;
 
 for($i = 0; $i < count($nutChinhSua); $i++) {
 	$keyboardButton = new KeyboardButton();
